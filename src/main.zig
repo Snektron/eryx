c9d4c405119c59daaed90eaece2a1d64d90336da
Robin Voetter <robin@voetter.nl>
2021-07-23 03:07:43 +0200

    Kernel call

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const build_options = @import("build_options");
 const c = @cImport({
+    @cInclude("malloc.h");
     @cInclude("futhark.h");
 });
 
@@ -65,7 +66,7 @@ fn parseOptions() !Options {
         } else if (std.mem.eql(u8, arg, "--futhark-debug")) {
             opts.futhark_debug = true;
         } else {
-            try stderr.print("Error: Unknown option {s}\n", .{arg});
+            try stderr.print("Error: Unknown option '{s}'\n", .{arg});
             return error.InvalidCmdline;
         }
     }
@@ -95,23 +96,77 @@ fn printHelp() !void {
         \\Usage: {s} [options...]
         \\Available options:
         \\-h --help           Show this message and exit.
-        \\--futhark-verbose   Enable futhark logging.
-        \\--futhark-debug     Enable futhark debug logging.
+        \\--futhark-verbose   Enable Futhark logging.
+        \\--futhark-debug     Enable Futhark debug logging.
         \\
         ++ backend_options
         , .{program_name},
     );
 }
 
-
 pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    const stderr = std.io.getStdErr().writer();
+
     const opts = parseOptions() catch {
-        try std.io.getStdErr().writer().print("See {s} --help\n", .{std.process.args().nextPosix().?});
+        try stderr.print("See {s} --help\n", .{std.process.args().nextPosix().?});
         std.os.exit(1);
     };
 
     if (opts.help) {
         try printHelp();
         return;
+    }
+
+    const cfg = c.futhark_context_config_new() orelse return error.OutOfMemory;
+    defer c.futhark_context_config_free(cfg);
+
+    c.futhark_context_config_set_logging(cfg, @boolToInt(opts.futhark_verbose));
+    c.futhark_context_config_set_debugging(cfg, @boolToInt(opts.futhark_debug));
+
+    switch (build_options.futhark_backend) {
+        .c => {},
+        .multicore => c.futhark_context_config_set_num_threads(cfg, opts.threads),
+        .opencl => {
+            if (opts.device_name) |name| {
+                c.futhark_context_config_set_device_name(cfg, name);
+            }
+
+            futhark_context_config_set_profiling(@boolToInt(opts.futhark_profile));
+        },
+    }
+
+    const ctx = c.futhark_context_new(cfg) orelse return error.OutOfMemory;
+    defer c.futhark_context_free(ctx);
+
+    const input = [_]i32{1, 2, 3, 4};
+    const src = c.futhark_new_i32_1d(ctx, &input, @intCast(i64, input.len)) orelse return error.OutOfMemory;
+    defer _ = c.futhark_free_i32_1d(ctx, src);
+
+    var dst: ?*c.futhark_i32_1d = null;
+    defer if (dst) |arr| {
+        _ = c.futhark_free_i32_1d(ctx, arr);
+    };
+
+    var err = c.futhark_entry_main(ctx, &dst, src);
+    err |= c.futhark_context_sync(ctx);
+    if (err != 0) {
+        return error.KernelFailed;
+    }
+
+    const len = c.futhark_shape_i32_1d(ctx, dst)[0];
+    const host_dst = try std.heap.page_allocator.alloc(i32, @intCast(usize, len));
+    _ = c.futhark_values_i32_1d(ctx, dst, host_dst.ptr);
+
+    try stdout.writeAll("Results:\n");
+    for (host_dst) |value| {
+        try stdout.print("{} ", .{value});
+    }
+    try stdout.writeByte('\n');
+
+    if (opts.futhark_profile) {
+        const report = c.futhark_context_report(ctx);
+        defer c.free(report);
+        try stderr.print("Profile report:\n{s}\n", .{std.mem.spanZ(report)});
     }
 }
